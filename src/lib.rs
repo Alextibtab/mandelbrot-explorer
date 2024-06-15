aa78305ea4fdb861dd5cf19e0f38d70800bcdd97
Alextibtab <14alexcockburn@gmail.com>
2024-06-15 18:06:12 +0100

    change adapter to be highperformance, formatting

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 // change sensitivity of mouse based on zoom level
 // or maybe consider switching to WASD controls
 // look at restructuring the code to be more modular
-// add egui 
+// add egui
 // write a struct for mandelbrot/fractal parameters
 // let these be changed by egui
 mod ui;
@@ -18,15 +18,16 @@ use winit::{
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct Mouse {              //            align(16) size(32)
-    x: f32,                 // offset(0)  align(4)  size(4)
-    y: f32,                 // offset(4)  align(4)  size(4)
-    drag: i32,              // offset(8)  align(4)  size(4)
-    px: f32,                // offset(12) align(4)  size(4)
-    py: f32,                // offset(16) align(4)  size(4)
-    centre_x: f32,          // offset(20) align(4)  size(4)
-    centre_y: f32,          // offset(24) align(4)  size(4)
-    _pad: u32,              // offset(28)           size(4)
+struct Mouse {
+    //            align(16) size(32)
+    x: f32,        // offset(0)  align(4)  size(4)
+    y: f32,        // offset(4)  align(4)  size(4)
+    drag: i32,     // offset(8)  align(4)  size(4)
+    px: f32,       // offset(12) align(4)  size(4)
+    py: f32,       // offset(16) align(4)  size(4)
+    centre_x: f32, // offset(20) align(4)  size(4)
+    centre_y: f32, // offset(24) align(4)  size(4)
+    _pad: u32,     // offset(28)           size(4)
 }
 
 impl Mouse {
@@ -73,13 +74,14 @@ impl Mouse {
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct ShaderUniform {      //            align(16) size(64) 
-    resolution: [f32; 2],   // offset(0)  align(8)  size(8)
-    iterations: i32,        // offset(8)  align(4)  size(4)
-    value: f32,             // offset(12) align(4)  size(4)
-    mouse: Mouse,           // offset(16) align(16) size(32)
-    axis_range: f32,        // offset(48) align(4)  size(4)
-    exponent: f32,          // offset(48) align(4)  size(4)
+struct ShaderUniform {
+    //            align(16) size(64)
+    resolution: [f32; 2], // offset(0)  align(8)  size(8)
+    iterations: i32,      // offset(8)  align(4)  size(4)
+    value: f32,           // offset(12) align(4)  size(4)
+    mouse: Mouse,         // offset(16) align(16) size(32)
+    axis_range: f32,      // offset(48) align(4)  size(4)
+    exponent: f32,        // offset(48) align(4)  size(4)
     _pad: [u32; 2],
 }
 
@@ -119,7 +121,6 @@ struct UiWrapper {
     interface: ui::Interface,
 }
 
-
 #[repr(C)]
 #[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
@@ -141,18 +142,27 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-1.0,  1.0, 0.0], colour: [1.0, 0.0, 0.0] },
-    Vertex { position: [-1.0, -1.0, 0.0], colour: [0.0, 1.0, 0.0] },
-    Vertex { position: [ 1.0, -1.0, 0.0], colour: [0.0, 0.0, 1.0] },
-    Vertex { position: [ 1.0,  1.0, 0.0], colour: [0.3, 0.4, 0.3] },
+    Vertex {
+        position: [-1.0, 1.0, 0.0],
+        colour: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-1.0, -1.0, 0.0],
+        colour: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [1.0, -1.0, 0.0],
+        colour: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        position: [1.0, 1.0, 0.0],
+        colour: [0.3, 0.4, 0.3],
+    },
 ];
 
-const INDICES: &[u16] = &[
-    0, 1, 3,
-    1, 2, 3
-];
+const INDICES: &[u16] = &[0, 1, 3, 1, 2, 3];
 
-struct State {  
+struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -179,25 +189,31 @@ impl State {
 
         let surface = unsafe { instance.create_surface(&window) }.unwrap();
 
-        let adapter = instance.request_adapter(
-            &wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
                 compatible_surface: Some(&surface),
                 force_fallback_adapter: false,
-            }
-        ).await.unwrap();
+            })
+            .await
+            .unwrap();
 
-        let (device, queue) = adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                features: wgpu::Features::empty(),
-                limits: wgpu::Limits::default(),
-                label: None,
-            },
-            None,
-        ).await.unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .unwrap();
 
         let surface_caps = surface.get_capabilities(&adapter);
-        let surface_format = surface_caps.formats.iter()
+        let surface_format = surface_caps
+            .formats
+            .iter()
             .copied()
             .find(|f| f.is_srgb())
             .unwrap_or(surface_caps.formats[0]);
@@ -219,19 +235,20 @@ impl State {
         });
 
         let mut shader_uniform = ShaderUniform::new();
-        shader_uniform.update_resolution(window.inner_size().width as f32, window.inner_size().height as f32);
-
-        let shader_buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("shader uniform"),
-                contents: bytemuck::cast_slice(&[shader_uniform]),
-                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-            }
+        shader_uniform.update_resolution(
+            window.inner_size().width as f32,
+            window.inner_size().height as f32,
         );
 
-        let shader_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            entries: &[
-                wgpu::BindGroupLayoutEntry {
+        let shader_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("shader uniform"),
+            contents: bytemuck::cast_slice(&[shader_uniform]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let shader_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[wgpu::BindGroupLayoutEntry {
                     binding: 0,
                     visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
                     ty: wgpu::BindingType::Buffer {
@@ -240,28 +257,23 @@ impl State {
                         min_binding_size: None,
                     },
                     count: None,
-                }
-            ],
-            label: Some("shader_bind_group_layout"),
-        });
+                }],
+                label: Some("shader_bind_group_layout"),
+            });
 
         let shader_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
             layout: &shader_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: shader_buffer.as_entire_binding(),
-                }
-            ],
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: shader_buffer.as_entire_binding(),
+            }],
             label: Some("shader_bind_group"),
         });
 
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[
-                    &shader_bind_group_layout,
-                ],
+                bind_group_layouts: &[&shader_bind_group_layout],
                 push_constant_ranges: &[],
             });
 
@@ -271,9 +283,7 @@ impl State {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[
-                    Vertex::desc(),
-                ],
+                buffers: &[Vertex::desc()],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
@@ -302,21 +312,17 @@ impl State {
             multiview: None,
         });
 
-        let vertex_buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Vertex Buffer"),
-                contents: bytemuck::cast_slice(VERTICES),
-                usage: wgpu::BufferUsages::VERTEX,
-            }
-        );
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
 
-        let index_buffer = device.create_buffer_init(
-            &wgpu::util::BufferInitDescriptor {
-                label: Some("Index Buffer"),
-                contents: bytemuck::cast_slice(INDICES),
-                usage: wgpu::BufferUsages::INDEX,
-            }
-        );
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
 
         let num_indices = INDICES.len() as u32;
 
@@ -330,7 +336,7 @@ impl State {
             winit_ctx,
             interface: ui::Interface::new(),
         };
-    
+
         Self {
             window,
             surface,
@@ -345,7 +351,7 @@ impl State {
             shader_uniform,
             shader_buffer,
             shader_bind_group,
-            ui_wrapper
+            ui_wrapper,
         }
     }
 
@@ -358,18 +364,27 @@ impl State {
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
-            self.shader_uniform.update_resolution(new_size.width as f32, new_size.height as f32);
+            self.shader_uniform
+                .update_resolution(new_size.width as f32, new_size.height as f32);
             self.surface.configure(&self.device, &self.config);
         }
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
-        self.ui_wrapper.winit_ctx.on_event(&self.ui_wrapper.ctx, event).consumed;
+        self.ui_wrapper
+            .winit_ctx
+            .on_event(&self.ui_wrapper.ctx, event)
+            .consumed;
 
         match event {
-            WindowEvent::CursorMoved { position, ..} => {
-                self.shader_uniform.mouse.update_position(position.x as f32, position.y as f32);
-                self.shader_uniform.mouse.drag_mouse(self.shader_uniform.resolution, self.shader_uniform.axis_range);
+            WindowEvent::CursorMoved { position, .. } => {
+                self.shader_uniform
+                    .mouse
+                    .update_position(position.x as f32, position.y as f32);
+                self.shader_uniform.mouse.drag_mouse(
+                    self.shader_uniform.resolution,
+                    self.shader_uniform.axis_range,
+                );
                 true
             }
 
@@ -379,7 +394,7 @@ impl State {
                 ..
             } => {
                 self.shader_uniform.mouse.set_drag();
-                true    
+                true
             }
 
             WindowEvent::MouseInput {
@@ -390,44 +405,54 @@ impl State {
                 self.shader_uniform.mouse.unset_drag();
                 true
             }
-            
-            WindowEvent::MouseWheel {
-                delta,
-                ..
-            } => {
-                match delta {
-                    MouseScrollDelta::LineDelta(_, y) => {
-                        if *y < 0.0 { self.shader_uniform.axis_range *= 1.05 } else { self.shader_uniform.axis_range *= 0.95 }
-                        true
+
+            WindowEvent::MouseWheel { delta, .. } => match delta {
+                MouseScrollDelta::LineDelta(_, y) => {
+                    if *y < 0.0 {
+                        self.shader_uniform.axis_range *= 1.05
+                    } else {
+                        self.shader_uniform.axis_range *= 0.95
                     }
-                    _ => false
+                    true
                 }
-            }
-            _ => false
+                _ => false,
+            },
+            _ => false,
         }
     }
 
     fn update(&mut self) {
         if self.shader_uniform.iterations != self.ui_wrapper.interface.iterations {
-            self.shader_uniform.update_iterations(self.ui_wrapper.interface.iterations);
+            self.shader_uniform
+                .update_iterations(self.ui_wrapper.interface.iterations);
         }
 
         if self.shader_uniform.value != self.ui_wrapper.interface.value {
-            self.shader_uniform.update_value(self.ui_wrapper.interface.value)
+            self.shader_uniform
+                .update_value(self.ui_wrapper.interface.value)
         }
 
         if self.shader_uniform.exponent != self.ui_wrapper.interface.exponent {
-            self.shader_uniform.update_exponent(self.ui_wrapper.interface.exponent);
+            self.shader_uniform
+                .update_exponent(self.ui_wrapper.interface.exponent);
         }
-        self.queue.write_buffer(&self.shader_buffer, 0, bytemuck::cast_slice(&[self.shader_uniform]));
+        self.queue.write_buffer(
+            &self.shader_buffer,
+            0,
+            bytemuck::cast_slice(&[self.shader_uniform]),
+        );
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
-        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            label: Some("Render Encoder"),
-        });
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
         let mut command_buffer = Vec::new();
 
         {
@@ -458,7 +483,7 @@ impl State {
                 pixels_per_point: self.ui_wrapper.winit_ctx.pixels_per_point(),
             };
 
-            let input  = self.ui_wrapper.winit_ctx.take_egui_input(&self.window);
+            let input = self.ui_wrapper.winit_ctx.take_egui_input(&self.window);
             let output = self.ui_wrapper.ctx.run(input, |ctx| {
                 self.ui_wrapper.interface.ui(ctx);
             });
@@ -473,9 +498,12 @@ impl State {
             let paint_jobs = self.ui_wrapper.ctx.tessellate(output.shapes);
 
             for (id, image_delta) in &texture_deltas.set {
-                self.ui_wrapper
-                    .wgpu_ctx
-                    .update_texture(&self.device, &self.queue, *id, image_delta);
+                self.ui_wrapper.wgpu_ctx.update_texture(
+                    &self.device,
+                    &self.queue,
+                    *id,
+                    image_delta,
+                );
             }
 
             for id in &texture_deltas.free {
@@ -508,7 +536,6 @@ impl State {
                 .render(&mut render_pass, &paint_jobs, &screen_descriptor);
 
             command_buffer.extend(ui_commands.into_iter());
-        
         }
 
         self.queue.submit(
@@ -536,20 +563,20 @@ pub async fn run() {
     let mut state = State::new(window).await;
 
     // Event Loop
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent { 
-                ref event, 
-                window_id 
-            } if window_id == state.window().id() => if !state.input(event) {
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == state.window().id() => {
+            if !state.input(event) {
                 match event {
                     WindowEvent::CloseRequested
-                    | WindowEvent::KeyboardInput { 
+                    | WindowEvent::KeyboardInput {
                         input:
                             KeyboardInput {
                                 state: ElementState::Pressed,
                                 virtual_keycode: Some(VirtualKeyCode::Escape),
-                                .. 
+                                ..
                             },
                         ..
                     } => *control_flow = ControlFlow::Exit,
@@ -562,20 +589,19 @@ pub async fn run() {
                     _ => {}
                 }
             }
-            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    Err(e) => eprintln!("{:?}", e),
-                }
-            }
-            Event::MainEventsCleared => {
-                state.window().request_redraw();
-            }
-            _ => {}
         }
+        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            state.window().request_redraw();
+        }
+        _ => {}
     });
 }
-
